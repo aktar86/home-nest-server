6cc86423f2eebbbc504729914eeb4486a79f2ced
aktar86 <amdaktar86@gmail.com>
2025-11-09 03:38:07 +0600

    express setup and properties apis sets

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.HN_USER}:${process.env.HN_PASS}@first-mongodb-project.ii5tnsm.mongodb.net/?appName=First-MongoDB-Project`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // create database
+    const db = client.db("properties_DB");
+    const propertiesCollection = db.collection("properties");
+
+    // get properties APIs here
+    app.get("/properties", async (req, res) => {
+      const cursor = propertiesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //post properties
+    app.post("/properties", async (req, res) => {
+      const newPropertiy = req.body;
+      const result = await propertiesCollection.insertOne(newPropertiy);
+      res.send(result);
+    });
+
+    //delete properties
+    app.delete("/properties/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await propertiesCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
